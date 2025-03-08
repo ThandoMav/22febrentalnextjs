9032cc0adb672a3d661bd45c1230801e5b1b9879
ThandoMav <eddymavweb@gmail.com>
2025-03-08 06:04:24 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,11 @@ import FeaturedBusinesses from '../components/FeaturedBusinesses';
 import CtaBgImgMzb from '../components/CtaBgImgMzb';
 import Post from '../models/Post';
 import Rental from '../models/Rental';
-import Bus from '../models/BusinessListing';
 import db from '../utils/db';
 import AboutMz from '../components/AboutMz';
 
 
-export default function Home({posts, buses, rentals }) {
+export default function Home({posts, rentals }) {
   return (
     <Layout title="Mzansiberg - Home Service listing and RSA Heritage conservation">
       {/*mzansiberg*/}
@@ -40,14 +39,12 @@ export default function Home({posts, buses, rentals }) {
 export async function getServerSideProps() {
   await db.connect();
   //const products = await Product.find().lean().limit(6);
-  const buses = await Bus.find().lean().limit(6);
   const posts = await Post.find().lean().limit(6);
   const rentals = await Rental.find().lean().limit(6);
  
   return {
     props: {
      // products: products.map(db.convertDocToObj),
-      buses: buses.map(db.convertDocToObj),
       rentals: rentals.map(db.convertDocToObj),
       posts: posts.map(db.convertDocToObj),
     },
